7eb6bf7b10e8e93b70652fe8f9034f5d2826da9a
Dimitris Vagiakakos <50751574+sv1sjp@users.noreply.github.com>
2024-03-24 19:59:45 +0200

    Create IdontwantMoney.sol

---
+++ Vulnerable Smart Contracts/IdontwantMoney.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract IdontwantMoney {/*
+I am an elephant. 
+I don't need your Ether
+ \
+    (""-,--"""""-.
+    |' :/ Greece |:
+  `-|;" : :,_,": |;
+    L   |_|   ||_|
+
+*/}        
+          
+
+
+
+```
