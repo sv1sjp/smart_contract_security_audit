08b9831b2ac18d2760d783dfd7ec4085ef2a784e
Dimitris Vagiakakos <50751574+sv1sjp@users.noreply.github.com>
2024-03-27 11:27:52 +0200

    Update AttackUnstoppable.sol

--- Attacking Smart Contracts/AttackUnstoppable.sol
+++ Attacking Smart Contracts/AttackUnstoppable.sol
@@ -1,5 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+// This smart contract implements an Attack Unstoppable challenge from Damn Vulnerable Defi.
+interface IPool{
+     function flashLoan(address receiver, address token, uint256 amount, bytes calldata data) external returns (bool);
+}
 contract AttackUnstoppable {
-   // This smart contract implements an Attack Unstoppable challenge from Damn Vulnerable Defi.
+   
     address private constant addressjs = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     
     constructor(address pool_address, address target) {
